345202c88058e5555010a3ecbd7726930a27067d
Evelyn O <evelynolives@gmail.com>
2022-11-27 14:56:55 -0300

    add hook to register form

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -1,22 +1,38 @@
+import React, {useState} from 'react';
 import "./RegisterForm.css"
 
 function RegisterForm() {
+
+const [values, setValues] = useState();
+console.log(values)
+
+const handleAddValues = (value) => {
+  setValues((prevValues) => ({
+    ...prevValues,
+    [value.target.name]: value.target.value,
+  }));
+};
+
     return (
    
         <div className="register-container">
           <h1 className="register-title">GATA STORE</h1>
           <input
-            type="text" name="name"
+            type="text" 
+            name="name"
             placeholder="Name"
             className="register-input"
+            onChange={handleAddValues}
           />
              <input
-            type="price" name="name"
+            type="price" 
+            name="name"
             placeholder="Price"
             className="register-input"
           />
              <input
-            type="category" name="Category"
+            type="category" 
+            name="Category"
             placeholder="Category"
             className="register-input"
           />
