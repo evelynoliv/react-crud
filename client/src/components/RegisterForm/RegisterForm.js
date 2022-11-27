c0ca83ca40f03886f79d2eb9bba48de5c1bf7121
Evelyn O <evelynolives@gmail.com>
2022-11-27 15:24:20 -0300

    update register form

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -1,46 +1,53 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./RegisterForm.css"
 
 function RegisterForm() {
 
-const [values, setValues] = useState();
-console.log(values)
+  const [values, setValues] = useState();
 
-const handleAddValues = (value) => {
-  setValues((prevValues) => ({
-    ...prevValues,
-    [value.target.name]: value.target.value,
-  }));
-};
 
-    return (
-   
-        <div className="register-container">
-          <h1 className="register-title">GATA STORE</h1>
-          <input
-            type="text" 
-            name="name"
-            placeholder="Name"
-            className="register-input"
-            onChange={handleAddValues}
-          />
-             <input
-            type="price" 
-            name="name"
-            placeholder="Price"
-            className="register-input"
-          />
-             <input
-            type="category" 
-            name="Category"
-            placeholder="Category"
-            className="register-input"
-          />
-          <button 
-          className="register-button">Register</button>
-        </div>
-      
-    );
+  const handleAddValues = (value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [value.target.name]: value.target.value,
+    }));
+  };
+
+  const handleClickButton = () => {
+    console.log(values);
   }
-  
-  export default RegisterForm;
+
+  return (
+
+    <div className="register-container">
+      <h1 className="register-title">GATA STORE</h1>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        className="register-input"
+        onChange={handleAddValues}
+      />
+      <input
+        type="text"
+        name="price"
+        placeholder="Price"
+        className="register-input"
+        onChange={handleAddValues}
+      />
+      <input
+        type="text"
+        name="category"
+        placeholder="Category"
+        className="register-input"
+        onChange={handleAddValues}
+      />
+      <button
+        onClick={handleClickButton}
+        className="register-button">Register</button>
+    </div>
+
+  );
+}
+
+export default RegisterForm;
