4bc751e4fd67dc10fc3771b31e80e76efc6f4c76
Evelyn O <evelynolives@gmail.com>
2022-12-28 12:50:04 -0300

    add mysql

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const dataBase = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    passqord: "Gata@212",
+    database: "reactcrud",
+});
+
+app.get('/', (req, res) => {
+    let SQL =
+        "INSERT INTO gata (name, cost, category) VALUES (001, Gata One, 1000, Lindeza)";
+
+    dataBase.query(SQL, (err, result) => {
+        console.log(err, "ih deu erro")
+    })
+});
+
 
 app.listen(3001, () => {
     console.log("server running")
 });
 
-app.get('/', (req, res) =>
-res.send("hello world!!!!!!"))
+
