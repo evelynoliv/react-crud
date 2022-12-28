921ca7d288b214480a86c46bfa0dd49152a5da0b
Evelyn O <evelynolives@gmail.com>
2022-12-28 14:35:53 -0300

    correct mysql permissions

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const mysql = require("mysql");
 
 const dataBase = mysql.createPool({
     host: "localhost",
-    user: "root",
-    passqord: "Gata@212",
+    user: "evelyn",
+    password: "Gata@212",
     database: "reactcrud",
 });
 
 app.get('/', (req, res) => {
     let SQL =
-        "INSERT INTO gata (name, cost, category) VALUES (001, Gata One, 1000, Lindeza)";
+        "INSERT INTO gata (idgata, name, cost, category) VALUES ('001', 'Gata One', '1000', 'Lindeza')";
 
     dataBase.query(SQL, (err, result) => {
         console.log(err, "ih deu erro")
